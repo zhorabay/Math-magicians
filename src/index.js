e37cf468e18385f30020205bfa1ef8e8fba0bbc6
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-09-12 15:53:36 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>s
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
