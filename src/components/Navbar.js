8b6431fe1bbf17e7eb946c8d1a8fed067b4947f5
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-09-19 11:53:53 +0300

    Give some style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Math Magicians</h1>
+      <h1 className="logo">Math Magicians</h1>
       <ul className="links">
         <Link to="/">Home</Link>
         <Link to="/calculator">Calculator</Link>
